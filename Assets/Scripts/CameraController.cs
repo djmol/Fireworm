e0408096fb9b42029916de4cb9ec2fc70cf51d4f
Daniel Molczyk <danielmolczyk@gmail.com>
2017-06-27 22:07:29 -0400

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target; // What camera is following
+	public float smoothing; // Dampening effect
+
+	Vector3 offset;
+	float lowY; // Lowest point camera will go
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+		lowY = transform.position.y - 50f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+		if (transform.position.y < lowY) {
+			transform.position = new Vector3 (transform.position.x, lowY, transform.position.z);
+		}
+	}
+}
---
+++ Assets/Scripts/PulseLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseLight : MonoBehaviour {
+
+	// Credit to Solomon Lutze
+	// http://www.gamasutra.com/blogs/SolomonLutze/20170601/299062/One_Month_Polish_Taking_a_Game_from_Ugly_to_Pretty_As_A_NonArtist.php
+	
+	public float flickerTime = 1.0f;  // time it takes to go from min to max intensity
+    public float initialIntensity = 1.0f; // minimum intensity
+    public float targetIntensity = 2.0f; // maximum intensity
+    float t = 0f;
+    Light light;
+
+    // Use this for initialization
+    void Start () {
+        light = GetComponent<Light> ();  // assign the light component to a reusable variable
+    }
+
+    void Update() {
+        light.intensity = Mathf.Lerp(initialIntensity, targetIntensity, t); // set intensity to t * 100% between initial and target intensity;
+        t +=  Time.deltaTime / flickerTime ; // increase t
+        if (t > 1.0f) // we have completed an intensity cycle
+        {
+            float temp = targetIntensity; // switch initial and target intensity
+            targetIntensity = initialIntensity;
+            initialIntensity = temp;
+            t = 0.0f; // reset t
+        }
+    }    
+}
